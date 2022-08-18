86bb4758b013b65625b0142354664eeb9e192260
InnocentusLime <InnocentusLime.help@gmail.com>
2022-08-18 13:32:56 +0300

    Added tiled project, added an experimental tileset, which is rewrite-compatible

--- bevy_game_bootstrap/assets/tilesets/activator_tiles.tsx
+++ bevy_game_bootstrap/assets/tilesets/activator_tiles.tsx
@@ -1,15 +1,15 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="activator_tiles" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+<tileset version="1.8" tiledversion="1.9.1" name="activator_tiles" tilewidth="32" tileheight="32" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
+ <tile id="0" type="ActivatorTile">
   <properties>
-   <property name="active" value="even"/>
+   <property name="active" propertytype="ActivationCondition" value="Even"/>
   </properties>
   <image width="32" height="32" source="../tiles/activator_even.png"/>
  </tile>
- <tile id="1">
+ <tile id="1" type="ActivatorTile">
   <properties>
-   <property name="active" value="odd"/>
+   <property name="active" propertytype="ActivationCondition" value="Odd"/>
   </properties>
   <image width="32" height="32" source="../tiles/activator_odd.png"/>
  </tile>
--- bevy_game_bootstrap/assets/tilesets/new_level_tiles.tsx
+++ bevy_game_bootstrap/assets/tilesets/new_level_tiles.tsx
@@ -1,25 +1,43 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="new_level_tiles" tilewidth="32" tileheight="32" tilecount="18" columns="1">
+<tileset version="1.8" tiledversion="1.9.1" name="new_level_tiles" tilewidth="32" tileheight="32" tilecount="18" columns="1">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="5" type="floor">
+ <tile id="5" type="LevelTile">
+  <properties>
+   <property name="ty" propertytype="LevelTileType" value="Floor"/>
+  </properties>
   <image width="32" height="32" source="../tiles/floor1.png"/>
  </tile>
- <tile id="6" type="floor">
+ <tile id="6" type="LevelTile">
+  <properties>
+   <property name="ty" propertytype="LevelTileType" value="Floor"/>
+  </properties>
   <image width="32" height="32" source="../tiles/floor2.png"/>
  </tile>
- <tile id="7" type="floor">
+ <tile id="7" type="LevelTile">
+  <properties>
+   <property name="ty" propertytype="LevelTileType" value="Floor"/>
+  </properties>
   <image width="32" height="32" source="../tiles/floor3.png"/>
  </tile>
- <tile id="8" type="floor">
+ <tile id="8" type="LevelTile">
+  <properties>
+   <property name="ty" propertytype="LevelTileType" value="Floor"/>
+  </properties>
   <image width="32" height="32" source="../tiles/floor4.png"/>
  </tile>
- <tile id="9" type="floor">
+ <tile id="9" type="LevelTile">
+  <properties>
+   <property name="ty" propertytype="LevelTileType" value="PlayerStart"/>
+  </properties>
   <image width="32" height="32" source="../tiles/player_start.png"/>
  </tile>
  <tile id="11">
   <image width="32" height="32" source="../tiles/exit.png"/>
  </tile>
- <tile id="12" type="conveyor">
+ <tile id="12" type="LevelTile">
+  <properties>
+   <property name="ty" propertytype="LevelTileType" value="Conveyor"/>
+  </properties>
   <image width="32" height="32" source="../tiles/conveyor1.png"/>
  </tile>
  <tile id="13">
@@ -31,7 +49,10 @@
  <tile id="15">
   <image width="32" height="32" source="../tiles/conveyor4.png"/>
  </tile>
- <tile id="16" type="fry">
+ <tile id="16" type="LevelTile">
+  <properties>
+   <property name="ty" propertytype="LevelTileType" value="Fry"/>
+  </properties>
   <image width="32" height="32" source="../tiles/fry0.png"/>
  </tile>
  <tile id="17">
