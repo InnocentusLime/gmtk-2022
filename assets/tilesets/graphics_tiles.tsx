c70dcbf9416bb34bed5371fda731aa44b23ec58b
InnocentusLime <innocentuslime.help@gmail.com>
2022-10-23 15:35:51 +0300

    Renamed types to sync up with Rust API

--- assets/tilesets/graphics_tiles.tsx
+++ assets/tilesets/graphics_tiles.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.9.2" name="graphics_tiles" tilewidth="32" tileheight="32" tilecount="21" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0" type="GraphicsTile">
+ <tile id="0" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type">{
   &quot;on_anim&quot;: [
@@ -14,121 +14,121 @@
   </properties>
   <image width="32" height="32" source="../tiles/conveyor1.png"/>
  </tile>
- <tile id="1" type="GraphicsTile">
+ <tile id="1" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/conveyor2.png"/>
  </tile>
- <tile id="2" type="GraphicsTile">
+ <tile id="2" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/conveyor3.png"/>
  </tile>
- <tile id="3" type="GraphicsTile">
+ <tile id="3" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/conveyor4.png"/>
  </tile>
- <tile id="4" type="GraphicsTile">
+ <tile id="4" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/exit.png"/>
  </tile>
- <tile id="5" type="GraphicsTile">
+ <tile id="5" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/floor1.png"/>
  </tile>
- <tile id="6" type="GraphicsTile">
+ <tile id="6" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/floor2.png"/>
  </tile>
- <tile id="7" type="GraphicsTile">
+ <tile id="7" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/floor3.png"/>
  </tile>
- <tile id="8" type="GraphicsTile">
+ <tile id="8" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/floor4.png"/>
  </tile>
- <tile id="9" type="GraphicsTile">
+ <tile id="9" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/fry0.png"/>
  </tile>
- <tile id="10" type="GraphicsTile">
+ <tile id="10" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/fry1.png"/>
  </tile>
- <tile id="11" type="GraphicsTile">
+ <tile id="11" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/fry2.png"/>
  </tile>
- <tile id="12" type="GraphicsTile">
+ <tile id="12" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/fry3.png"/>
  </tile>
- <tile id="13" type="GraphicsTile">
+ <tile id="13" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/fry4.png"/>
  </tile>
- <tile id="14" type="GraphicsTile">
+ <tile id="14" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/fry5.png"/>
  </tile>
- <tile id="15" type="GraphicsTile">
+ <tile id="15" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/fry6.png"/>
  </tile>
- <tile id="16" type="GraphicsTile">
+ <tile id="16" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/fry7.png"/>
  </tile>
- <tile id="17" type="GraphicsTile">
+ <tile id="17" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/spin0.png"/>
  </tile>
- <tile id="18" type="GraphicsTile">
+ <tile id="18" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/spin1.png"/>
  </tile>
- <tile id="19" type="GraphicsTile">
+ <tile id="19" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
   <image width="32" height="32" source="../tiles/spin2.png"/>
  </tile>
- <tile id="20" type="GraphicsTile">
+ <tile id="20" type="GraphicsTileBundle">
   <properties>
    <property name="animation_type" value="null"/>
   </properties>
