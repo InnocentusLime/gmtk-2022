aa03deabdc2c3414de537b9f20ff4d78fec44468
InnocentusLime <InnocentusLime.help@gmail.com>
2022-12-31 10:58:04 +0200

    testing

--- bevy_credits/src/lib.rs
+++ bevy_credits/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use bevy::prelude::*;
+
+pub enum SectionEntry {
+    Subsection(CreditSection),
+    SimpleEntry {
+        role: String,
+        name: String,
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct CreditSection {
+    name: String,
+    entries: Vec<SectionEntry>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct CreditsAsset {
+    sections: Vec<CreditSection>,
+}
+
+impl CreditsAsset {
+    pub fn build(commands: &mut Commands) {
+        commands.spawn(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(40.0), Val::Percent(100.0)),
+                flex_direction: FlexDirection::ColumnReverse,
+                align_items: AlignItems::Center,
+                margin: UiRect {
+                    top: Val::Px(5.0),
+                    ..default()
+                },
+                ..default()
+            },
+            color: Color::rgba(0.0, 0.15, 0.15, 0.1).into(),
+            ..default()
+        });
     }
 }
